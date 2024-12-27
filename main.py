916258a24ab6c68fd59a8dc00c87a66588d7c464
Miguel <falconmiguel.2004@gmail.com>
2024-12-27 18:32:49 +0100

    Section 3: 1-12 Types, conversions and constants

--- main.py
+++ main.py
@@ -4,4 +4,12 @@
 This is a multiple 
 line comment
 """
-print("Hello World")
+greeting = "hello "
+print(greeting + "Miguel")
+
+PI = 3.1415 #UpperCase to know it is a constant
+
+name: str = "Miguel"
+number: float = 12.532879
+
+print(str(number)) #It prints as a String
