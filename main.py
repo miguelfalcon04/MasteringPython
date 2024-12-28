f1105e0e3d0b4378cf7753dac96be977a6a72d9a
Miguel <falconmiguel.2004@gmail.com>
2024-12-28 14:25:08 +0100

    Section 4. Break and Continue, Pass, Loop Else

--- main.py
+++ main.py
@@ -31,3 +31,61 @@ a = 0
 while a < 10:
         print(a)
         a += 1
+
+print()
+
+# Break & continue
+print('BREAK & CONTINUE')
+
+for i in range(10):
+        print(i)
+        if i == 5:
+                break
+
+i = 0
+while i<3:
+        print(i)
+        if i== 1:
+                break
+
+        i += 1
+
+for i in range(5):
+        if i == 3:
+                continue
+
+        print(i)
+
+print()
+
+# Pass 
+print('PASS')
+
+x , y = 10, 20
+if x > y:
+        pass
+
+print()
+
+# Loop Else
+print('LOOP ELSE')
+
+for i in range(5):
+        print(i, end=' ')
+
+        if i == 2:
+                break
+else:
+        print('Succes')
+
+print()
+
+t = 0
+while t < 3:
+        print(t, end=' ')
+        t+=1
+
+        if t == 2:
+                break
+else:
+        print('Succes')
