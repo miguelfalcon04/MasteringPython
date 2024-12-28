ef4ed371ba6ac32289345526d577014fcbdd467f
Miguel <falconmiguel.2004@gmail.com>
2024-12-28 13:57:44 +0100

    Section 4. If else, For, While

--- main.py
+++ main.py
@@ -1,87 +1,33 @@
-# Basic Operators
-print(2+2)
-print(2*2)
-print(2-2)
-print(10/3)
-print(10//3)
-print(10%3)
-print(10 ** 3)
+# If else
+print('IF ELSE')
+text = 'cd'
 
-print(10==10)
-print(10!=10)
-print(10==5)
-print(10!=5)
+if text == 'Hello':
+        print('Bot: Hello!')
+elif text == 'Bye':
+        print('Bot: Goodbye!')
+else:
+        print('I did not understand' )
 
-a = 20
-b = 40
-
-print( a<b and 4>6 )
-print( a<b or 5>6 )
-
-numbers = [1,2,3,4]
-print(1 in numbers)
-
-# Strings
-var = "text"
-print(f"1 {var} 2")
-
-# List
-people: list[str] = ['Mario', 'Luigi', 'Peach', 'Toad']
-pokemon: list[str] = ['Pikacu', 'Celebi']
-people[0] = 'Chill Guy'
-people.insert(2,'!!!')
-people.append('To the end')
-people.remove('Peach')
-people.pop(0)
-people.sort()
-people.clear()
-people.extend(pokemon)
-print(people)
-
-# Tuples
-print('TUPLES')
-tple: tuple = ('Mario' , 'Luigi', 'Peach')
-tple_list: list[str] = list[tple]
-tple_tuple : tuple = tuple[tple_list]
-print(tple)
+print('Succes') if 3 > 2 else print('failure')
 print()
 
-# Sets
-print('SETS')
+# For loop
+print('FOR LOOP')
+people: list[str] =['Mario', 'Luigi', 'Peach', 'Toad'] 
 
-items: set = {'Apple', 'Banana', 10, True}
-items2: set = {'kiwi', 40, 'Banana', 'Apple'}
-items.add('orange')
-items.update(['carrot', 15])
-items.remove('Apple')
-items.discard('Banana')
+for person in people:
+        print(f'Hello, {person}')
 
-new_set = items.union(items2)
-new_set = items | items2
-items |= items2
-items.intersection_update(items2)
-print(items)
+for number in range(10):
+        print(number)
+
 print()
 
-# Dictionaries
-print('DICTIONARIES')
-users = {'User1': 'Mario123', 
-        'User2': 'Luigi456',
-        'fruits':{'apple':10,
-                'banana': 20}}
+# While Loop
+print('WHILE LOOP')
 
-user1 = users['User1']
-print(user1)
-x = list(users.keys())
-print(x)
-y = list(users.values())
-print(y)
-users['User1'] = 'Toad789'
-all = users.items()
-print(all)
-
-users.update({'hello': 123})
-users.pop('hello')
-print(users)
-print(users['fruits'])
-print(users['fruits']['apple'])
+a = 0
+while a < 10:
+        print(a)
+        a += 1
