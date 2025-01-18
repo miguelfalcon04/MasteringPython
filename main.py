17a79b565c264665a06bcacf72d88a1064b1e7f2
Miguel <falconmiguel.2004@gmail.com>
2025-01-18 20:27:29 +0100

    Section 14. Handling & Caching JSON

--- main.py
+++ main.py
@@ -1106,13 +1106,66 @@
 #     print(text.read())
 
 # # DELETING FILES
-import os
+# import os
 
 # if os.path.exists('hello.text'):
     # os.remove('hello.text')
 
-for item in os.listdir('sample_folder'):
-    if os.path.exists('sample_folder/' + item):
-        os.remove('sample_folder/' + item)
+# for item in os.listdir('sample_folder'):
+#     if os.path.exists('sample_folder/' + item):
+#         os.remove('sample_folder/' + item)
 
-os.rmdir('sample_folder')
+# os.rmdir('sample_folder')
+
+# # HANDLING JSON
+import json
+
+def get_json(file: str) -> dict:
+    with open(file, 'r') as file:
+        data: str = file.read()
+        actual: dict = json.loads(data)
+        return actual
+
+sample_data: dict = get_json('data.json') 
+print(sample_data)
+
+sample: dict = {'name': 'Elon', 'age': 12, 'has_mars': False}
+# sample_json = json.dumps(sample)
+# print(sample_json)
+
+with open('sample.json', 'w') as file:
+    json.dump(sample, file)
+
+# # CACHING JSON
+import json
+import requests
+
+def fech_data(*, update: bool= False, json_cache: str, url: str) -> dict:
+    if update:
+        json_data = None
+    else:
+        try:
+            with open(json_cache, 'r') as file:
+                json_data = json.load(file)
+                print('Fetched data from the local cache!')
+
+        except(FileNotFoundError, json.JSONDecodeError) as e:
+            print(f'No local cache found ({e})')
+            json_data = None
+    
+    if not json_data:
+        print('Fetching new json data... (Creating local cache)')
+        json_data = requests.get(url).json()
+        with open(json_cache, 'w') as file:
+            json.dump(json_data, file)
+    return json_data
+
+api_url: str = 'https://dummyjson.com/comments'
+cache_file: str = 'comments.json'
+
+data: dict = fech_data(
+    update= False,
+    json_cache=cache_file,
+    url=api_url
+)
+print(data)
