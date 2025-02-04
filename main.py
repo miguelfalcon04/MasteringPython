d83dfab01989e13f1e462281f572b74e99f08ab2
Miguel <falconmiguel.2004@gmail.com>
2025-02-04 20:33:13 +0100

    Section 20 Tips & Tricks. F-Strings, Tuples, Flat Lists, Elipses, #NOQA

--- main.py
+++ main.py
@@ -1713,8 +1713,8 @@
 # if __name__ == '__main__':
 #     main()
 
-# # # # # # # SECTION 19 Projects # # # # # # # 
 
+# # # # # # # SECTION 19 Projects # # # # # # # 
 
 
 # # # # # # # IMPOSSIBLE QUIZ # # # # # # # 
@@ -1823,54 +1823,133 @@
 
 # # # # # # # ACCURATE CHAT BOT # # # # # # # 
 
-import json
-import re
-import random_responses
+# import json
+# import re
+# import random_responses
 
-def load_json(file):
-    with open(file) as bot_responses:
-        print(f'Loaded "{file}" succesfully!')
-        return json.load(bot_responses)
+# def load_json(file):
+#     with open(file) as bot_responses:
+#         print(f'Loaded "{file}" succesfully!')
+#         return json.load(bot_responses)
     
 
-response_data: dict = load_json('responses.json')
+# response_data: dict = load_json('responses.json')
 
-def get_response(input_str: str):
-    split_message: list[str] = re.split(r'\s+|[,;?!.-]\s*', input_str.lower())
-    score_list: list[int] = []
+# def get_response(input_str: str):
+#     split_message: list[str] = re.split(r'\s+|[,;?!.-]\s*', input_str.lower())
+#     score_list: list[int] = []
     
-    for response in response_data:
-        required_score: int = 0
-        response_score: int = 0
-        required_words: list[str] = response['required_words']
+#     for response in response_data:
+#         required_score: int = 0
+#         response_score: int = 0
+#         required_words: list[str] = response['required_words']
         
-        if required_words:
-            for word in split_message:
-                if word in required_words:
-                    required_score += 1
+#         if required_words:
+#             for word in split_message:
+#                 if word in required_words:
+#                     required_score += 1
         
-        if required_score == len(required_words):
-            for word in split_message:
-                if word in response['user_input']:
-                    response_score += 1
+#         if required_score == len(required_words):
+#             for word in split_message:
+#                 if word in response['user_input']:
+#                     response_score += 1
             
-        score_list.append(response_score)
+#         score_list.append(response_score)
     
-    best_response: int = max(score_list)
-    response_index: int = score_list.index(best_response)
+#     best_response: int = max(score_list)
+#     response_index: int = score_list.index(best_response)
     
-    if input_str == "":
-        return "Please type somethin"
+#     if input_str == "":
+#         return "Please type somethin"
     
-    if best_response != 0:
-        return response_data[response_index]['bot_response']
+#     if best_response != 0:
+#         return response_data[response_index]['bot_response']
     
-    return random_responses.get_random_response()
+#     return random_responses.get_random_response()
 
-def main():
-    while True:
-        user_input: str = input('You: ')
-        print('Bot:', get_response(user_input))
+# def main():
+#     while True:
+#         user_input: str = input('You: ')
+#         print('Bot:', get_response(user_input))
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+
+# # # # # # # SECTION 20 TIPS & TRICKS # # # # # # # 
+
+# # # # # # # F-Strings are powerful # # # # # # #
+
+text: str = 'SAMPLE'
+print(f'{text:20}: hello')
+print(f'{text:<20}: hello')
+print(f'{text:>20}: hello')
+print(f'{text:^20}: hello')
+
+print(f'{text:_<20}: hello')
+print(f'{text:_>20}: hello')
+print(f'{text:_^20}: hello')
+
+number:float = 10_000.123456
+print(f'{number:.2f}')
+print(f'{number:,.2f}')
+
+number2: int = 1_000_000
+print(f'{number2:.0e}')
+
+# # # # # # # Tuples & Type Hinting # # # # # # #
+
+numbers: tuple[int | str, ...] = (12, 'hello', 1234)
+
+# # # # # # # Flattening Lists # # # # # # #
+from typing import Iterable
+
+people: list = [['Mario', 'Luigi'], ['James', 'Bob']]
+new_list: list[str] = []
+for names in people:
+    for name in names:
+        new_list.append(name)
+
+new_list2: list[str] = [name
+                        for names in people
+                        for name in names]
+
+
+def flatten(iterable: Iterable) -> list:
+    l: list = []
+    for item in iterable:
+        if isinstance(item, Iterable) and not isinstance(item, str):
+            l.extend(flatten(item))
+        else:
+            l.append(item)
+    return l
+
+print(new_list)
+print(new_list2)
+print(flatten(people))
+
+# # # # # # # Elipses # # # # # # #
+print(...)
+
+# # # # # # # Linting # # # # # # #
+# pylint main.py in console
+
+def do_something():
+    print('Hello world')
+
+class Fruit:
+    def __init__(self):
+        print('I am a fruit')
+    
+    def explode(self):
+        pass
+
+fruit: Fruit = Fruit()
+do_something()
+
+
+# # # # # # # #noqa # # # # # # #
+import math, random # NOQA
+
+math.sqrt(10)
+random.random()
